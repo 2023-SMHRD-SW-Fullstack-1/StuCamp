9ef600e70c7af4187b7aa5e5c2dffea7a421b66c
orthh <witchicken2@gmail.com>
2023-07-31 14:29:51 +0900

    docs : 서버폴더구조생성

---
+++ server/models/index.js
@@ -0,0 +1,25 @@
+const Sequelize = require("sequelize");
+const User = require("./user");
+
+// 어떤 db, 계정
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config")[env];
+
+// sequelize 객체 생성(객체 내부에는 DB 연결 정보 가지고 있음)
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+// sequelize, model
+const db = {};
+
+db.sequelize = sequelize;
+db.User = User;
+
+User.init(sequelize);
+User.associate(db); //관계설정했을시
+
+module.exports = db;
