db86faf20a6843c8c7cd0e5378d85f1776703ecb
orthh <witchicken2@gmail.com>
2023-07-31 17:38:31 +0900

    docs : routes 생성

--- server/app.js
+++ server/app.js
@@ -1,19 +1,26 @@
-const express = require('express')
-const {sequelize} = require('./models')
-const indexRouter = require('./routes/index')
-const app = express()
+const express = require("express");
+const { sequelize } = require("./models");
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/user");
+const recordRouter = require("./routes/record");
+const app = express();
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
 // sequelize 설정
-sequelize.sync({force : false})
-.then(()=>console.log('DB연결성공!'))
-.catch((err)=>{console.log(err);});
+sequelize
+  .sync({ force: false })
+  .then(() => console.log("DB연결성공!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 //routes 연결
-app.use('/', indexRouter)
+app.use("/", indexRouter);
+app.use("/user", userRouter);
+app.use("/record", recordRouter);
 
-app.set('port', process.env.PORT||8888)
-app.listen(app.get('port'), ()=>{
-    console.log(app.get('port'), '번 포트에서 서버연결 기다리는중...');
-})
+app.set("port", process.env.PORT || 8888);
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 서버연결 기다리는중...");
+});
