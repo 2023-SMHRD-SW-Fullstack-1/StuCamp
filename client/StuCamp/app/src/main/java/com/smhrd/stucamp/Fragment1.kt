fd12863d7d8934ef2f20a59d833c7243f40e336f
서현록 <sh0501sh11@gmail.com>
2023-07-31 18:20:33 +0900

    hyunrok feed created

--- client/StuCamp/app/src/main/java/com/smhrd/stucamp/Fragment1.kt
+++ client/StuCamp/app/src/main/java/com/smhrd/stucamp/Fragment1.kt
@@ -1,21 +1,45 @@
 package com.smhrd.stucamp
+// 현록
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.smhrd.stucamp.VO.FeedVO
 
 class Fragment1 : Fragment() {
 
+    lateinit var rc : RecyclerView
+    lateinit var btnWriteAct : Button
  override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        var view = inflater.inflate(R.layout.fragment_1, container, false)
 
-     var view = inflater.inflate(R.layout.fragment_1, container, false)
+        rc = view.findViewById(R.id.rcFeed)
+        btnWriteAct = view.findViewById(R.id.btnWriteAct)
 
+//        btnWriteAct.setOnClickListener(){
+//         val intent = Intent(requireActivity(), FeedWriteActivity::class.java)
+//         startActivity(intent)
+//        }
 
+        val feedList = ArrayList<FeedVO>()
+
+        feedList.add(FeedVO("hihi", 3, "하이하이"))
+        feedList.add(FeedVO("ID1", 2, "하이하이1"))
+        feedList.add(FeedVO("ID2", 5, "하이하이2"))
+        feedList.add(FeedVO("ID3", 3, "하이하이3"))
+
+        val adapter = FeedAdapter(feedList, requireActivity())
+        rc.layoutManager = LinearLayoutManager(requireActivity())
+        rc.adapter = adapter
 
 
      return view
---
+++ client/StuCamp/app/src/main/java/com/smhrd/stucamp/VO/FeedVO.kt
@@ -0,0 +1,9 @@
+package com.smhrd.stucamp.VO
+
+import java.math.BigInteger
+
+data class FeedVO (var feedId : String,  var feedLikeCnt : Int, var feedContent : String)
+
+
+//var feedImgPath : String,
+//var edtComment : String
