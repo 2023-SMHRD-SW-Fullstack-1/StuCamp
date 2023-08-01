9a662d8e89ff58a970537bf103028c83491e57d5
Yeongseok <108849327+zrston@users.noreply.github.com>
2023-08-01 12:11:37 +0900

    230801 지도 기능 출력

--- client/StuCamp/app/src/main/java/com/smhrd/stucamp/Fragment3.kt
+++ client/StuCamp/app/src/main/java/com/smhrd/stucamp/Fragment3.kt
@@ -1,23 +1,32 @@
 package com.smhrd.stucamp
 
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import net.daum.mf.map.api.MapView
+
 
 class Fragment3 : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        var view = inflater.inflate(R.layout.fragment_3, container, false)
+    ): View {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_3, container, false)
 
+        // Initialize the MapView
+        val mapView = MapView(requireActivity())
 
+        // Add the MapView to the container
+        val mapViewContainer = view.findViewById<ViewGroup>(R.id.map_view)
+        mapViewContainer.addView(mapView)
 
-
+        // Return the view
         return view
     }
-
 }
