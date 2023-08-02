3063b42a3f39b0644efe941608bfca55debe1dd2
Yeongseok <108849327+zrston@users.noreply.github.com>
2023-08-02 12:51:14 +0900

    230802 지도 기능 출력(현재위치 실시간 표시 미완성,)

--- client/StuCamp/app/src/main/java/com/smhrd/stucamp/Fragment3.kt
+++ client/StuCamp/app/src/main/java/com/smhrd/stucamp/Fragment3.kt
@@ -13,6 +13,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.MapFragment
 import com.smhrd.stucamp.databinding.Fragment3Binding
@@ -44,39 +45,11 @@ class Fragment3 : Fragment() {
         val mapViewContainer = view.findViewById<ViewGroup>(R.id.map_view)
         mapViewContainer.addView(mapView)
 
-        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(33.41, 126.52), 4, true);
+        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(35.14982, 126.919953), 1, true);
 
-        fun getCurrentLocation() {
-            val permissionCheck = ContextCompat
-                .checkSelfPermission(requireContext(), ACCESS_FINE_LOCATION)
-            if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
-                val lm = requireContext()
-                    .getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-                try {
-                    val userCurLocation = lm
-                        .getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-
-                    val uLatitude = userCurLocation!!.latitude
-                    val uLogitude = userCurLocation.longitude
-                    val uCurPosition = MapPoint.mapPointWithGeoCoord(uLatitude, uLogitude)
+        
 
 
-                    mapView.setMapCenterPoint(uCurPosition, true)
-                } catch (e: NullPointerException) {
-                    Log.e("LOCATION_ERROR", e.toString())
-
-                    ActivityCompat.finishAffinity(requireActivity())
-
-                    val intent = Intent(context, MapFragment::class.java)
-                    startActivity(intent)
-                    System.exit(0)
-                }
-            } else {
-                Toast.makeText(requireContext(), "위치 권한이 없습니다.", Toast.LENGTH_SHORT).show()
-                requestPermissions(REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE)
-            }
-        }
 
         // Return the view
         return view
